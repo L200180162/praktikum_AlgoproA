a47b00afb4cf958c156d0ae01f283a5ade89066e
L200180162 <44260139+L200180162@users.noreply.github.com>
2018-11-15 21:37:30 +0700

    Add files via upload

---
+++ rhnm.py
@@ -0,0 +1,12 @@
+a = str(input('Masukkan Nama Saudara : '))
+b = float (input('Pukul Berapa Sekarang : '))
+if 06.00 >= b:
+    print ('Selamat Petang' + ' ' + a)
+elif 10.00 >=b :
+    print ('Selamat Pagi' + ' ' + a)
+elif 13.00 >=b :
+    print ('Selamat Siang' + ' ' + a)
+elif 16.00 >=b :
+    print ('Selamat Sore' + ' ' + a)
+elif 20.00 >=b :
+    print ('Selamat Malam' + ' ' + a)
